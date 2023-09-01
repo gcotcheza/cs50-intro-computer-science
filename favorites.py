bc3eff0f8004baf10ad52eef017b8e317e2ebf4e
Ghie Cotcheza <89792810+gcotcheza@users.noreply.github.com>
2023-09-01 12:32:20 +0000

    Fri, Sep 1, 2023, 2:32 PM +02:00

--- favorites.py
+++ favorites.py
@@ -2,6 +2,7 @@ import csv
 
 with open("favorites.csv", "r") as file:
     reader = csv.reader(file)
+    # next(row) # will skip the column header
     for row in reader:
         favorite = row[1]
         print(favorite)
