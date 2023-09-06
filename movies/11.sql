8eba273ee9bcbd3e5ff72f2f1f4f470bab829410
Ghie Cotcheza <89792810+gcotcheza@users.noreply.github.com>
2023-09-06 08:06:45 +0000

    Wed, Sep 6, 2023, 10:06 AM +02:00

---
+++ movies/11.sql
@@ -0,0 +1,6 @@
+SELECT m.title FROM movies AS m
+    JOIN ratings AS r ON r.movie_id = m.id
+    JOIN stars AS s ON s.movie_id = m.id
+    JOIN people AS p ON s.person_id = p.id
+    WHERE p.name = "Chadwick Boseman"
+    ORDER BY r.rating desc;
