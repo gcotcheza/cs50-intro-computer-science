bc0ded3604a40da76be4547d8f555e6a257ba0fe
Ghie Cotcheza <89792810+gcotcheza@users.noreply.github.com>
2023-09-13 11:52:03 +0000

    Wed, Sep 13, 2023, 1:52 PM +02:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,5 +1,12 @@
-SELECT m.title FROM movies AS m
-    JOIN stars AS s ON m.id = s.movie_id
-    JOIN people AS p ON s.person_id = p.id
-    WHERE name = "Kevin Bacon"
-        AND birth = "1958";
+SELECT DISTINCT p.name
+FROM movies AS m
+JOIN stars AS s1 ON m.id = s1.movie_id
+JOIN people AS p ON s1.person_id = p.id
+WHERE m.id IN (
+    SELECT DISTINCT m2.id
+    FROM movies AS m2
+    JOIN stars AS s2 ON m2.id = s2.movie_id
+    JOIN people AS kb ON s2.person_id = kb.id
+    WHERE kb.name = 'Kevin Bacon' AND kb.birth = 1958
+)
+AND p.name != 'Kevin Bacon';
