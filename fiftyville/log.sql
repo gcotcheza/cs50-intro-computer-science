cb0b52622dcabb64f3ce177d28f3ddec793611b5
Ghie Cotcheza <89792810+gcotcheza@users.noreply.github.com>
2023-09-13 13:09:33 +0000

    Wed, Sep 13, 2023, 3:09 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -2,5 +2,6 @@
 -- See how it looks the rows and data on the crime_scene_reports:
    select * from crime_scene_reports;
 -- Find specific crime scene description:
-    sel
+    select description from crime_scene_reports where month = 7 and day = 28 and street = "Humphrey Street";
+
 
